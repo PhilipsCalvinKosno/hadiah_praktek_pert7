1602a3ee4d65654c63e556aded23605ea2fdd081
PhilipsCalvinKosno <72179377+PhilipsCalvinKosno@users.noreply.github.com>
2020-11-17 19:28:02 +0800

    Add files via upload

---
+++ hadiah teori/Polimorfisme/main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main{
+	public static void main(String[]args){
+		
+		double vol;
+		Scanner input = new Scanner(System.in);
+		System.out.print("masukkan nilai jari-jari :");
+		vol = input.nextInt();
+	
+		hitung h = new hitung(vol);
+		volume v = new hitung(vol);
+		
+		h.hasil();	
+		
+	}
+	}
